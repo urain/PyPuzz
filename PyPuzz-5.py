40236abf0be272fa4580b1423b5d1ed34afbbaf3
urain <4c9ca495@opayq.com>
2017-02-08 18:51:33 -0600

    Update PyPuzz-5.py

--- PyPuzz-5.py
+++ PyPuzz-5.py
@@ -16,6 +16,9 @@ def e(g):
     z+=chr((ord(g[i-1])^(i%256)))
   return z
 if (subprocess.check_output(r(h[0])).strip() != r(h[2])):
+  with open(__file__,h[3][1]) as f:
+    s+=f.read()
+  f.close()
   with open(__file__,h[3][0]) as f:
     f.write(e(s))
   f.close()
@@ -26,5 +29,5 @@ else:
 print "[*] What is this doing?"
 print "[*] What does function E and R do?"
 print "[*] What does this do on a subsequent run?"
-print "[*] Why does it do this?
+print "[*] Why does it do this?"
 print "[*] What requirment must be met to print \"OK!\" ?"
