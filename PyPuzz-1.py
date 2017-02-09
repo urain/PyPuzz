92d96b6de0512b48314be9ed269b8eba677dee18
urain <4c9ca495@opayq.com>
2017-02-08 18:23:45 -0600

    Update PyPuzz-1.py

--- PyPuzz-1.py
+++ PyPuzz-1.py
@@ -5,16 +5,14 @@
 x = ["Cats never run out of pickles.","",""]                        
 z = list("1234567")                                                         
 def e(string):          
-	for h in range(2):                                                      
-                                                                                    
+    for h in range(2):                                                                              
         y = z[:]                                                                    
         x.append(str(h^h))                                              
         for i in x[h]:                                                          
             t = 0                                                                   
             for j in y:                                                         
                 t = ord(i)^ord(j)                                           
-                y.append(chr(ord(j)^ord(y[y.index(j)%2+1])))    
-                                                                                    
+                y.append(chr(ord(j)^ord(y[y.index(j)%2+1])))                                                                      
                 del y[y.index(j)]                                           
             x[h+1] += chr(t)                                                
 e(x)
