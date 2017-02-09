e1bf915952962dfd18ef56c7af3277d4f78b650d
urain <4c9ca495@opayq.com>
2017-02-08 18:21:25 -0600

    Update PyPuzz-1.py

--- PyPuzz-1.py
+++ PyPuzz-1.py
@@ -5,19 +5,21 @@
 x = ["Cats never run out of pickles.","",""]						
 z = list("1234567")															
 def e(string):			
-for h in range(2):																																			
-	y = z[:]																	
-	x.append(str(h^H))												
-	for i in x[h]:															
-		t = 0																	
-		for j in y:															
-			t = ord(i)^ord(j)											
-			y.append(chr(ord(j)^ord(y[y.index(j%2+1]))) 																			
-			del y[y.index(j)]											
-		x[h+1] += chr(t)												
+	for h in range(2):														
+																					
+		y = z[:]																	
+		x.append(str(h^h))												
+		for i in x[h]:															
+			t = 0																	
+			for j in y:															
+				t = ord(i)^ord(j)											
+				y.append(chr(ord(j)^ord(y[y.index(j)%2+1]))) 	
+																					
+				del y[y.index(j)]											
+			x[h+1] += chr(t)												
 e(x)
 print "ORIGINAL:\t" + x[0]
 print "OUTPUT:\t"   + x[1]
 
-print "What is this doing?"
-print "Can you reverse the output to the original?"
+print "[*] What is this doing?"
+print "[*] Is it possible to reverse the output back to the original? If so, code a solution."
